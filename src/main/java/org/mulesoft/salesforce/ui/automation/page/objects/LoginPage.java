1c4ae9b8e60e11413950515dd3b222ede93b8747
Gowtham <gowthampotluri@gmail.com>
2022-01-09 00:06:09 +0530

    added code to implement ui automation

---
+++ src/main/java/org/mulesoft/salesforce/ui/automation/page/objects/LoginPage.java
@@ -0,0 +1,39 @@
+package org.mulesoft.salesforce.ui.automation.page.objects;
+
+import org.mulesoft.salesforce.ui.automation.WebUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    private WebDriver driver;
+
+    @FindBy(how = How.ID, using = "username")
+    private WebElement userName;
+
+    @FindBy(how = How.ID, using = "password")
+    private WebElement password;
+
+    @FindBy(how = How.ID, using = "Login")
+    private WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterUserName(String uName) {
+        WebUtil.enterTextIntoTextBox(userName, uName);
+    }
+
+    public void enterPassword(String pswd) {
+        WebUtil.enterTextIntoTextBox(password, pswd);
+    }
+
+    public void clickLogin() {
+        WebUtil.clickButton(loginButton);
+    }
+}
